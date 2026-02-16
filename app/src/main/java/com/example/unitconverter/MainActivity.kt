0fea004b975148c0d5c02d64fceb1d890db8ef0a
W0LF12AM <bayui4742@gmail.com>
2026-02-16 20:50:39 +0700

    initial (not really initial)

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,123 @@
+package com.example.unitconverter
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel by lazy { ViewModelProvider(this).get(MainViewModel::class.java) }
+    private val buttonCelcius by lazy {findViewById<Button>(R.id.btn_celcius)}
+    private val buttonFahrenheit by lazy {findViewById<Button>(R.id.btn_fahrenheit)}
+    private val tvInput by lazy {findViewById<TextView>(R.id.tv_input)}
+    private val tvOutput by lazy {findViewById<TextView>(R.id.tv_output)}
+    private val btnSwap by lazy {findViewById<Button>(R.id.btn_swap)}
+
+    private val btnOne by lazy {findViewById<Button>(R.id.btn_1)}
+    private val btnTwo by lazy {findViewById<Button>(R.id.btn_2)}
+    private val btnThree by lazy {findViewById<Button>(R.id.btn_3)}
+    private val btnClear by lazy {findViewById<Button>(R.id.btn_c)}
+
+    private val btnFour by lazy {findViewById<Button>(R.id.btn_4)}
+    private val btnFive by lazy {findViewById<Button>(R.id.btn_5)}
+    private val btnSix by lazy {findViewById<Button>(R.id.btn_6)}
+    private val btnPlus by lazy {findViewById<Button>(R.id.btn_plus)}
+
+    private val btnSeven by lazy {findViewById<Button>(R.id.btn_7)}
+    private val btnEight by lazy {findViewById<Button>(R.id.btn_8)}
+    private val btnNine by lazy {findViewById<Button>(R.id.btn_9)}
+    private val btnMinus by lazy {findViewById<Button>(R.id.btn_minus)}
+
+    private val btnZero by lazy {findViewById<Button>(R.id.btn_0)}
+    private val btnComa by lazy {findViewById<Button>(R.id.btn_coma)}
+    private val btnOk by lazy {findViewById<Button>(R.id.btn_ok)}
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        observeInput()
+        observeOutput()
+
+        btnOne.setButtonClickListener("1")
+        btnTwo.setButtonClickListener("2")
+        btnThree.setButtonClickListener("3")
+        btnClear.setOnClickListener { viewModel.clearInput() }
+
+
+        btnFour.setButtonClickListener("4")
+        btnFive.setButtonClickListener("5")
+        btnSix.setButtonClickListener("6")
+        btnPlus.setButtonClickListener("+")
+
+        btnSeven.setButtonClickListener("7")
+        btnEight.setButtonClickListener("8")
+        btnNine.setButtonClickListener("9")
+        btnMinus.setButtonClickListener("-")
+
+        btnZero.setButtonClickListener("0")
+        btnComa.setButtonClickListener(".")
+
+        btnSwap.setOnClickListener {
+            animateInputIndicator()
+            viewModel.switchInputUnit()
+            viewModel.resetInputAndOutput()
+        }
+
+        btnOk.setOnClickListener { viewModel.convertUnit() }
+    }
+
+    private fun observeInput() {
+        viewModel.input.observe(this) {
+            input -> tvInput.text = input
+        }
+    }
+
+    private fun observeOutput() {
+        viewModel.output.observe(this) {
+            output -> tvOutput.text = output
+        }
+    }
+
+    private fun animateInputIndicator() {
+        //get initial potition
+        val initialY1 = buttonCelcius.y
+        val initialY2 = buttonFahrenheit.y
+
+        //animate button celcius
+        buttonCelcius.animate()
+            .alpha(0f)
+            .y(initialY2)
+            .setDuration(300)
+            .withEndAction {
+                buttonCelcius.y = initialY2
+                buttonCelcius.alpha = 1f
+            }
+
+        buttonFahrenheit.animate()
+            .alpha(0f)
+            .y(initialY1)
+            .setDuration(300)
+            .withEndAction {
+                buttonFahrenheit.y = initialY1
+                buttonFahrenheit.alpha = 1f
+            }
+    }
+
+    private fun Button.setButtonClickListener(value: String) {
+        setOnClickListener {
+            viewModel.setInput(value)
+        }
+    }
+}
